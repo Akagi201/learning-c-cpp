aa82991dff7489a4e9d2c6d7ec2e678abc058e52
Akagi201 <akagi201@gmail.com>
2017-01-11 15:55:16 +0800

    feat(cgen): update main.cc

--- cgen/cppclass/cpplib/main.cc
+++ cgen/cppclass/cpplib/main.cc
@@ -1,11 +1,18 @@
-#include "my_class.h"
 #include <iostream>
+#include "my_class.h"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    MyClass *c = new MyClass();
-    c->int_set(3);
-    cout << c->int_get() << endl;
-    delete c;
+	{
+		MyClass c = MyClass();
+		c.int_set(3);
+		cout << c.int_get() << endl;
+	}
+	{
+		MyClass* c = new MyClass();
+		c->int_set(3);
+		cout << c->int_get() << endl;
+		delete c;
+	}
 }
