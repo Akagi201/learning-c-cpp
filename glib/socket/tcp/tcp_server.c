2e9424104823c77ef6a486471d878240dbc133f9
Akagi201 <akagi201@gmail.com>
2017-05-18 14:37:34 +0800

    feat(glib): async read

--- glib/socket/tcp/tcp_server.c
+++ glib/socket/tcp/tcp_server.c
@@ -4,16 +4,47 @@
 #include <gio/gio.h>
 #include <glib.h>
 
-/* this function will get called everytime 25a client attempts to connect */
+#define BLOCK_SIZE (1024)
+#define PORT (8327)
+
+struct ConnData {
+	GSocketConnection *connection;
+	char message[BLOCK_SIZE];
+};
+
+void message_ready(GObject *source_object, GAsyncResult *res,
+                   gpointer user_data) {
+	GInputStream *istream = G_INPUT_STREAM(source_object);
+	GError *error = NULL;
+	struct ConnData *data = user_data;
+	int count;
+
+	count = g_input_stream_read_finish(istream, res, &error);
+
+	if (count == -1) {
+		g_error("Error when receiving message");
+		if (error != NULL) {
+			g_error("%s", error->message);
+			g_clear_error(&error);
+		}
+	}
+
+	g_message("Message was: %s", data->message);
+	g_object_unref(G_SOCKET_CONNECTION(data->connection));
+	g_free(data);
+}
+
+/* this function will get called everytime a client attempts to connect */
 static gboolean incoming_callback(GSocketService *service,
                                   GSocketConnection *connection,
                                   GObject *source_object, gpointer user_data) {
 	g_message("Received Connection from client!");
 	GInputStream *istream =
 	    g_io_stream_get_input_stream(G_IO_STREAM(connection));
-	gchar message[1024] = {};
-	g_input_stream_read(istream, message, 1024, NULL, NULL);
-	g_print("Message was: \"%s\"\n", message);
+	struct ConnData *data = g_new(struct ConnData, 1);
+	data->connection = g_object_ref(connection);
+	g_input_stream_read_async(istream, data->message, sizeof(data->message),
+	                          G_PRIORITY_DEFAULT, NULL, message_ready, data);
 	return FALSE;
 }
 
@@ -25,8 +56,7 @@ int main(int argc, char **argv) {
 	/* create the new socketservice */
 	service = g_socket_service_new();
 
-	ret = g_socket_listener_add_inet_port(G_SOCKET_LISTENER(service),
-	                                      8327, /* your port goes here */
+	ret = g_socket_listener_add_inet_port(G_SOCKET_LISTENER(service), PORT,
 	                                      NULL, &error);
 
 	/* don't forget to check for errors */
