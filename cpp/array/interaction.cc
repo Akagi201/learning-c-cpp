998370e4fdd59c867f3b1e2b8840f38500c5c634
Akagi201 <akagi201@gmail.com>
2018-05-11 13:58:25 +0800

    feat(array): add interaction

--- cpp/array/interaction.cc
+++ cpp/array/interaction.cc
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-int interaction_count(vector<int>& a1, vector<int>& a2) {
+int interaction_count(const vector<int>& a1, const vector<int>& a2) {
   unordered_set<int> s1(a1.begin(), a1.end());
   int count = 0;
   for (const int& i:a2) {
@@ -16,16 +16,8 @@ int interaction_count(vector<int>& a1, vector<int>& a2) {
 }
 
 int main() {
-  vector<int> a1;
-  vector<int> a2;
-
-  a1.push_back(1);
-  a1.push_back(2);
-  a1.push_back(3);
-
-  a2.push_back(2);
-  a2.push_back(3);
-  a2.push_back(4);
+  vector<int> a1({1,2,3});
+  vector<int> a2({2,3,4});
 
   cout << interaction_count(a1, a2) << endl;
 
